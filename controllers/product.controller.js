a0c97e871e031287920313c42bfa1574a029fafe
dinukah99 <dinukahamarasinghe@gmail.com>
2024-04-14 23:39:34 +0530

    get product by id function

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -10,3 +10,18 @@ const getProducts = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+// Get Product By ID
+const getProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        if (!product) {
+            res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json(product);
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
