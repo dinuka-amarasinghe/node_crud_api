ea1088f65b12f80c2f692ee8689b54b077faaafd
dinukah99 <dinukahamarasinghe@gmail.com>
2024-04-14 23:37:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+gitconst express = require('express');
+const mongoose = require('mongoose');
+const productRoute = require('./routes/product.route.js');
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+// MongoDB Connection
+mongoose.connect('mongodb+srv://dinukahamarasinghe:LGyvz0Uqx2y7eF5k@node-api.vcuintw.mongodb.net/?retryWrites=true&w=majority&appName=Node-API')
+    .then(() => {
+        console.log('Connected to database!');
+    })
+    .catch(() => {
+        console.log('Connection failed!');
+    });
+
+// Routes
+app.use("/api/products", productRoute);
+
+
