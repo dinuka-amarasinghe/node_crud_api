bfe1292014c98359cf580224e43ee9d34da9b217
dinukah99 <dinukahamarasinghe@gmail.com>
2024-04-17 23:58:40 +0530

    changes

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const productRoute = require('./routes/product.route.js');
+const port = 3000;
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
 
 // MongoDB Connection
